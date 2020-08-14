15cc730d3451d21e5534e89678066c399162735f
V-G-spec <62442188+V-G-spec@users.noreply.github.com>
2020-08-14 13:23:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   token: process.env.TOKEN,
-  amounts: (process.env.AMOUNTS && process.env.AMOUNTS.split(' ')) || [3, 10, 60] // list of amounts available (in seconds)
+  amounts: (process.env.AMOUNTS && process.env.AMOUNTS.split(' ')) || [3, 10, 60, 300, 600, 900, 1800] // list of amounts available (in seconds)
 }
